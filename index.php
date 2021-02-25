a1890c93fa436447bd5aa7ba68047c1f6680de9d
Karel Asselman <asselman.karel@gmail.com>
2021-02-25 12:07:50 +0100

    all db classes loaded in bootstrap file intead of index

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
-//require 'helpers/common.php';
+require 'helpers/common.php';
 require 'models/Task.php';
-require 'database/Connection.php';
+require 'bootstrap.php';
 
-
-$pdo = Connection::Make();
-
-$statement = $pdo->prepare('select * from todos');
-$statement->execute();
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-
+$tasks = $query->SelectAll('todos', 'Task');
 
 // dd($tasks);
 
